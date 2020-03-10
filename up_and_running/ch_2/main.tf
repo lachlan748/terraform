b43973016f1a6afee7fa31a4e7f3e2b43d1fb2f8
lachlanfalconer <lachlanfalconer@pop-os.localdomain>
2020-03-10 11:27:15 +0000

    Updated main.tf and added variables file

--- up_and_running/ch_2/main.tf
+++ up_and_running/ch_2/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
    profile = "lachlan.falconer"
 }
 
-resource "aws_instance" "example" {
-  ami 				= "ami-0c55b159cbfafe1f0"
-  instance_type 		= "t2.micro"
-  vpc_security_group_ids 	= [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id                      = "ami-0c55b159cbfafe1f0"
+  instance_type                 = "t2.micro"
+  security_groups	        = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -15,13 +15,32 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  tags = {
-    Name = "my_example_ec2_via_terraform"
+  # required when using launch configuration:
+  lifecycle {
+    create_before_destroy	= true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration		= aws_launch_configuration.example.name
+  # grab the subnet info from aws using data sources:
+  vpc_zone_identifier		= data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size			= 2
+  max_size			= 10
+
+  tag {
+    key				= "Name"
+    value			= "terraform_asg_example"
+    propagate_at_launch		= true
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "test-sg"
+  name = var.instance_security_group_name
 
   ingress {
     from_port   = var.server_port
@@ -31,13 +50,93 @@ resource "aws_security_group" "instance" {
   }
 }
 
-variable "server_port" {
-  description 	= "Web server port"
-  type 		= number
-  default	= 8080
+data "aws_vpc" "default" {
+  default = true
 }
 
-output "public_ip" {
-  value		= aws_instance.example.public_ip
-  description	= "The public IP of my ec2 instance"
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+output "alb_dns_name" {
+  value		= aws_lb.example.dns_name
+  description	= "The domain name of the load balancer"
+}
+
+resource "aws_lb" "example" {
+  name				= var.alb_name
+  load_balancer_type		= "application"
+  subnets			= data.aws_subnet_ids.default.ids
+  security_groups		= [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn		= aws_lb.example.arn
+  port				= 80
+  protocol			= "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type		= "text/plain"
+      message_body		= "404: page not found"
+      status_code		= 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+
+  name = var.alb_name
+
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    field  = "path-pattern"
+    values = ["*"]
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = var.alb_security_group_name
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
